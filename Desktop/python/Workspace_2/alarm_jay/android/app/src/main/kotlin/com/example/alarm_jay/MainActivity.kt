eabd10570397270795a6c5e110b03b68d8cdc25d
Jayyo <gracekim7765@gmail.com>
2023-09-15 20:48:54 +0900

    '848'

--- Desktop/python/Workspace_2/alarm_jay/android/app/src/main/kotlin/com/example/alarm_jay/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.alarm_jay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Desktop/python/Workspace_2/alarm_jay/lib/AlarmInfo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class AlarmInfo {
-  TimeOfDay time;
-  String tone;
-
-  AlarmInfo({required this.time, required this.tone});
-}
